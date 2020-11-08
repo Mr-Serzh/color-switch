2f1d95aed4230937105b65939e0f0a133a931267
Serhii Hayvoronskyi <hayvoronskyi@ukr.net>
2020-11-08 02:48:03 +0200

    rezult

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
-];
+import colors from './colors';
 
-// генерации случайного числа
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('button[data-action="start"]');
+const stopBtn = document.querySelector('button[data-action="stop"]');
+
+let timer;
+
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+
+// генерация случайного числа
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function onStart() {
+    timer = setInterval(() => {
+		bodyEl.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+	}, 1000);
+    startBtn.disabled = true;
+};
+
+function onStop() {
+    clearInterval(timer);
+    startBtn.disabled = false;
 };
